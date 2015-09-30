29b279aa48ca33f915b2416e0540ccb756be515a
jhosler0793 <jhosler0793@gmail.com>
2015-09-30 12:28:38 -0500

    Removed .gradle folder and batch compile files.

--- onepress.bat
+++
@@ -1,7 +0,0 @@
-call gradle clean build jacocoTestReport
-echo "Running acceptance tests..."
-call runacceptance.bat
-explorer "file:///%~dp0build/reports/tests/index.html"
-explorer "file:///%~dp0build/reports/jacoco/test/html/index.html"
-explorer "file:///%~dp0acceptance/report.html"
-pause
--- runacceptance.bat
+++
@@ -1,4 +0,0 @@
-cd acceptance
-javac -cp .;..\build\classes\main;C:\RobotFramework\robotframework-2.8.5.jar ArgumentParserKeywords.java
-java -cp .;..\build\classes\main;C:\RobotFramework\robotframework-2.8.5.jar org.robotframework.RobotFramework ArgumentParserTests.txt
-cd ..
